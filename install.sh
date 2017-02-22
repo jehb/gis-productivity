d503f1ba8c6f733145e29fe65067f96f57200683
Jason Baker <jehb@redhat.com>
2017-02-21 23:28:20 -0500

    added some sample nodered

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Start with everything up to date.
-dnf update -y
+# Or comment this out if you're in a hurry when testing (probably not a best practice).
+
+# dnf update -y
 
 # Let's NodeJS / npm
 
@@ -14,3 +16,7 @@ npm install -g --unsafe-perm node-red
 # We also want QGIS and Docker
 
 dnf install qgis docker -y
+
+# And then, let's use pip (the python installer) to install Jupyter Notebooks
+
+pip3 install jupyter
